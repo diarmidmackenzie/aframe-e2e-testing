54831f564ac8994dbb535097687e827934dfbfc9
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2022-06-19 12:44:50 +0100

    Clarifying comment

--- aframe-e2e-testing.js
+++ aframe-e2e-testing.js
@@ -1,3 +1,11 @@
+// This module is equivalent to index.js.
+// It exists to allow Plywright config & scripts *within* this package to access
+// the packe's own exports.
+// They do so like this (example from the test scripts).
+//
+// module.paths.push(`${module.path}/../..`
+// const { A } = require('aframe-e2e-testing');
+
 const A = require('./src/aframe-pw-utils.js')
 const extend = require('./src/expect-extensions.js')
 
